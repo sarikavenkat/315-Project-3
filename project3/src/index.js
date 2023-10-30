60c17a40727e91346f17bc75f01c4cff05ec4dd5
Sarika <sarikavenkat21@gmail.com>
2023-10-29 23:00:40 -0500

    started home page

--- project3/src/index.js
+++ project3/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+function App() {
+  return (
+    <div>
+      <nav>
+        <div className="store-name">Sweet Paris Crêperie & Café</div>
+        <ul className="nav-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/order">Order</a></li>
+          <li><a href="/login">Login</a></li>
+        </ul>
+      </nav>
+      <div className="content">
+        {/* Your page content goes here */}
+      </div>
+    </div>
+  );
+}
+
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
