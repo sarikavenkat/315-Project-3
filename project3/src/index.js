6b8b90b4998488527e4d75d4dbdcbcc2c1af7e9b
Sarika <sarikavenkat21@gmail.com>
2023-10-29 23:09:02 -0500

    nav bar

--- project3/src/index.js
+++ project3/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css'; 
 
 function App() {
   return (
     <div>
-      <nav>
+      <div className="store-header">
         <div className="store-name">Sweet Paris Crêperie & Café</div>
+      </div>
+      <nav className="navbar">
         <ul className="nav-links">
           <li><a href="/">Home</a></li>
           <li><a href="/menu">Menu</a></li>
