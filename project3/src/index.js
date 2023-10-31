a8ad67cd4d56acf5fe714f7d048ce48f3112d7fa
simonvaradaraj <s.varadaraj03@gmail.com>
2023-10-30 21:53:55 -0500

    routing works

--- project3/src/index.js
+++ project3/src/index.js
@@ -1,58 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home'
+
 import './style.css'; 
 // import App from "./app";
 
+// Components for different routes
+// const Home = () => <div>Home</div>;
+
+// Make these individual js/jsx files in the pages folder. Then put them into the index file like I did with home.
+const Menu = () => <div>Menu</div>;
+const Order = () => <div>Order</div>;
+const Login = () => <div>Login</div>;
+
 function App() {
   return (
-    <div className="app-container">
-      <div className="store-header">
-        <div className="store-name">Sweet Paris Crêperie & Café</div>
-      </div>
-      <nav className="navbar">
-        <ul className="nav-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/">Menu</a></li>
-          <li><a href="/">Order</a></li>
-          <li><a href="/">Login</a></li>
-        </ul>
-      </nav>
-      <div className="homepage-images">
-        <img src="/sp1.jpg" alt="sweet paris pic 1" className="image" />
-        <img src="/sp2.jpg" alt="sweet paris pic 2" className="image" />
-        <img src="/sp3.jpg" alt="sweet paris pic 3" className="image" />
-        <img src="/sp4.jpg" alt="sweet paris pic 4" className="image" />
-      </div>
-      <div className="content">
-      <div className="about-section">
-          <h2>Who OUI Are:</h2>
-          <p>
-            Our mission is to revive the art of eating crêpes!
-          </p>
-          <p>
-            Sweet Paris was first opened in Rice Village in 2012. Within 10 years, Sweet Paris has turned into one of the best fast casual hotspots in various parts of Houston, Austin, San Antonio, College Station, Miami, Woodbury, and international resort destinations.
-          </p>
-          <p>
-            Every day - and every bite - is an opportunity to savor all that’s good in the world. Although crêpes are known for being French, we have introduced hints of other cultures into this versatile dish that can be enjoyed any time of the day!
-          </p>
-          <p>
-          While some may see crêpes as overly simple, we wish to elevate the crêpe with our fresh quality ingredients, interesting flavor profiles, and celebratory atmosphere. From breakfast to dinner, brunch to late dessert, we strive to serve delicious food with a helping of outstanding hospitality. Our beloved guests choose Sweet Paris for routine get-togethers and special milestones alike.
-          </p>
+    <BrowserRouter>
+      <div className="app-container">
+        <div className="store-header">
+          <div className="store-name">Sweet Paris Crêperie & Café</div>
         </div>
+        <nav className="navbar">
+          <ul className="nav-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/order">Order</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
-      <div className="contact-bar">
-        <p>CONTACT US!</p>
-        <p>Address: 123 Main Street, College Station, TX</p>
-        <p>Phone: (123) 456-7890</p>
-        <p>Email: info@sweetpariscollegestation.com</p>
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
 const root = document.getElementById('root');
 ReactDOM.createRoot(root).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
