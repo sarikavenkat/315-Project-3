18188c86a383ba5027847f736872ad7d924b02e4
Paul <pauljwbae@gmail.com>
2023-11-14 10:02:19 -0600

    Skeleton for Manager.js + dependency stuff
    I created the HTML Skeleton and handle() functions for Manager.js, and now all I need to do is the logic and wiring to the db. MAKE SURE TO REINSTALL PACKAGE-LOCK.JSON PLEASE! mac does not work unless I create a new thing every time and I think it messes with the windows consoles (?)

--- project3/src/pages/employeepages/Manager.js
+++ project3/src/pages/employeepages/Manager.js
@@ -3,23 +3,63 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./managerstyle.css";
 
 const Manager = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const { state } = location;
-    const managerName = state && state.managerName;
+  const [employee, setEmployee] = useState([]);
+  useEffect(() => {
+    // Fetch employee data from API and update state
+    // Placeholder: Replace the following line with actual API call
+    // fetchEmployeeData().then(data => setEmployee(data));
+  }, []);
 
-    const handleLogout = () => {
-        navigate('/emplogin'); 
-      };
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { state } = location;
+  const managerName = state && state.managerName;
 
-    return (
-        <div>
-        <h1>
-            Welcome, Manager: {managerName} 
-        </h1>
-        <button onClick={handleLogout}>Logout</button>
-        </div>
-    )
+  // I want to be able to track employee work schedule
+  const handleCheckEmployeeSchedules = (index) => {
+    // Placeholder: Implement logic to track employee work schedule
+    console.log("Tracking employee work schedule for index:", index);
+  };
+
+  // I want to be able to view the order history
+  const handleViewOrderHistory = () => {
+    // Placeholder: Implement logic to view order history
+    console.log("Viewing order history");
+  };
+
+  // I want to be able to remove the order items table from the db
+  const handleRemoveOrderItems = () => {
+    // Placeholder: Implement logic to remove order items from the database
+    console.log("Removing order items from the database");
+  };
+
+  // I want to be able to append the order specifics onto the order history
+  const handleAppendToOrderHistory = () => {
+    // Placeholder: Implement logic to append order specifics to order history
+    console.log("Appending order specifics to order history");
+  };
+
+  const handleLogout = () => {
+    navigate('/emplogin');
+  };
+
+  return (
+    <div>
+      <h1>
+        Welcome, Manager: {managerName}
+      </h1>
+      <div className="employee-times">
+        <p>Total Price: ${/* Add logic to calculate total price */}</p>
+        <button onClick={() => handleCheckEmployeeSchedules(/* Add employee index */)}>
+          Add Item to Cart
+        </button>
+        <button onClick={handleViewOrderHistory}>View Order History</button>
+        <button onClick={handleRemoveOrderItems}>Remove Order Items</button>
+        <button onClick={handleAppendToOrderHistory}>Append to Order History</button>
+      </div>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 };
 
 export default Manager;
