76fe775db5c76ab9730c1fa2cc6418da93ef5677
Paul <pauljwbae@gmail.com>
2023-11-14 18:18:19 -0600

    Finished barebones of view order history
    Can now see the order History on manager GUI

--- project3/src/pages/employeepages/Manager.js
+++ project3/src/pages/employeepages/Manager.js
@@ -4,111 +4,151 @@ import "./managerstyle.css";
 
 const Manager = () => {
   const [employee, setEmployee] = useState([]);
-  const [menuItems, setMenuItems] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  
+  const [orders, setOrders] = useState([]);
+  const [filteredEmployeeData, setFilteredData] = useState([]);
+  const [orderList, setOrderList] = useState([]);
+  const [index, setIndex] = useState(0);
 
   const navigate = useNavigate();
   const location = useLocation();
   const { state } = location;
   const managerName = state && state.managerName;
 
-    useEffect(() => {
-        fetchEmployees();
-        fetchMenuItems();
-    }, []);
+  useEffect(() => {
+    fetchEmployees();
+    fetchOrders();
+  }, []);
 
-    const fetchMenuItems = async () => {
-        try {
-          const response = await fetch("http://localhost:5000/api/items");
-          if (response.ok) {
-            console.log("fetchmenuItems");
-            const data = await response.json();
-            setMenuItems(data);
-          } else {
-            throw new Error("Failed to fetch menu items");
-          }
-        } catch (error) {
-          console.error("Error fetching menu items", error);
-        }
-      };
-
-    const fetchEmployees = async () => {
-        try {
-            const response = await fetch("http://localhost:5000/api/employees");
-            if (response.ok) {
-              console.log("fetchemployees");
-              const data = await response.json();
-              //console.log(data[0]);
-              setEmployee(data);
-              //console.log(employee)
-            } else {
-              throw new Error("Failed to fetch menu items");
-            }
-          } catch (error) {
-            console.error("Error fetching menu items", error);
-          }
-      };
-
-      
-
-  // I want to be able to track employee work schedule
-  const handleCheckEmployeeSchedules = (index) => {
-    const filteredEmployee = [];
-    employee.forEach(element => {
-        filteredEmployee.push({ ['name']: element['name'], ['id']: element['id'], ['next_work_day']: element['next_work_day'].slice(0, -14), ['start_time']: element['start_time'], ['end_time']: element['end_time']}); 
-    });
-    setFilteredData(filteredEmployee);
-    //console logs
-    console.log("filtered data: ");
-    console.log(filteredData);
-    console.log("Tracking employee work schedule for index:", index);
-    
+  const fetchOrders = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/orders");
+      if (response.ok) {
+        console.log("fetchorders");
+        const data = await response.json();
+        setOrders(data);
+      } else {
+        throw new Error("Failed to fetch orders");
+      }
+    } catch (error) {
+      console.error("Error fetching orders", error);
+    }
   };
 
-  // I want to be able to view the order history
+  const fetchEmployees = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/employees");
+      if (response.ok) {
+        console.log("fetchemployees");
+        const data = await response.json();
+        setEmployee(data);
+      } else {
+        throw new Error("Failed to fetch menu items");
+      }
+    } catch (error) {
+      console.error("Error fetching menu items", error);
+    }
+  };
+
+  const handleCheckEmployeeSchedules = () => {
+    const filteredEmployee = [];
+    employee.forEach((element) => {
+      filteredEmployee.push({
+        name: element.name,
+        id: element.id,
+        next_work_day: element.next_work_day.slice(0, -14),
+        start_time: element.start_time,
+        end_time: element.end_time,
+      });
+    });
+    setFilteredData(filteredEmployee);
+    console.log("Tracking employee work schedule");
+  };
+
   const handleViewOrderHistory = () => {
-    // Placeholder: Implement logic to view order history
+    const subsetStartIndex = index;
+    const subsetEndIndex = index + 20;
+    const tempOrderList = [];
+
+    if (subsetEndIndex < orders.length && subsetStartIndex >= 0) {
+      for (let i = subsetStartIndex; i < subsetEndIndex; i++) {
+        tempOrderList.push({
+          orderid: orders[i].orderid,
+          orderdatetime:
+            orders[i].orderdatetime.slice(0, -14) +
+            " " +
+            orders[i].orderdatetime.slice(-13, -5),
+          customerid: orders[i].customerid,
+          price:"$" +orders[i].price,
+          calories: orders[i].calories
+        });
+      }
+      setOrderList(tempOrderList);
+    }
+
+    console.log(orderList);
+    console.log(orders.length);
     console.log("Viewing order history");
   };
 
-  // I want to be able to remove the order items table from the db
   const handleRemoveOrderItems = () => {
-    // Placeholder: Implement logic to remove order items from the database
     console.log("Removing order items from the database");
   };
 
-  // I want to be able to append the order specifics onto the order history
   const handleAppendToOrderHistory = () => {
-    // Placeholder: Implement logic to append order specifics to order history
     console.log("Appending order specifics to order history");
   };
 
   const handleLogout = () => {
-    navigate('/emplogin');
+    navigate("/emplogin");
   };
 
-  const [showTable, setShowTable] = useState(false);
+  const [showEmployeeTable, setShowEmployeeTable] = useState(false);
+  const [showOrderHistory, setShowOrderHistory] = useState(false);
 
   return (
     <div>
       <h1>Welcome, Manager: {managerName}</h1>
+
       <div className="employee-times">
-        <p>Total Price: ${/* Add logic to calculate total price */}</p>
-        <button onClick={() => {
-          handleCheckEmployeeSchedules(/* Add employee index */);
-          setShowTable(!showTable); // Show the table when the button is clicked
-        }}>
+        <button
+          onClick={() => {
+            handleCheckEmployeeSchedules();
+            setShowEmployeeTable(!showEmployeeTable);
+          }}
+        >
           Check Employee Schedules
         </button>
-        <button onClick={handleViewOrderHistory}>View Order History</button>
-        <button onClick={handleRemoveOrderItems}>Remove Order Items</button>
-        <button onClick={handleAppendToOrderHistory}>Append to Order History</button>
       </div>
-      <button onClick={handleLogout}>Logout</button>
+      <div className="OrderHistory">
+        <button
+          onClick={() => {
+            handleViewOrderHistory();
+            setShowOrderHistory(!showOrderHistory);
+          }}
+        >
+          View Order History
+        </button>
+      </div>
+
+      <button onClick={handleRemoveOrderItems}>Remove Order Items</button>
+      <button onClick={handleAppendToOrderHistory}>
+        Append to Order History
+      </button>
+
+      <button onClick={() => setIndex((prevIndex) => prevIndex - 20)}>
+        
+        Previous Orders
+      </button>
+      <button onClick={() => {
+      setIndex((prevIndex) => prevIndex + 20);
+      }
+      }>
+        
+        Next Orders
+      </button>
 
       {/* Display the table only when showTable is true */}
-      {showTable && (
+      {showEmployeeTable && !showOrderHistory && (
         <table>
           <thead>
             <tr>
@@ -120,13 +160,38 @@ const Manager = () => {
             </tr>
           </thead>
           <tbody>
-            {filteredData.map((employee, index) => (
+            {filteredEmployeeData.map((employee, index) => (
               <tr key={index}>
                 <td>{employee.name}</td>
                 <td>{employee.id}</td>
                 <td>{employee.next_work_day}</td>
                 <td>{employee.start_time}</td>
                 <td>{employee.end_time}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
+      {showOrderHistory && !showEmployeeTable && (
+        <table>
+          <thead>
+            <tr>
+              <th>Order ID</th>
+              <th>customer ID</th>
+              <th>Date/Time</th>
+              <th>Price</th>
+              <th>Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderList.map((orders, index) => (
+              <tr key={index}>
+                <td>{orders.orderid}</td>
+                <td>{orders.customerid}</td>
+                <td>{orders.orderdatetime}</td>
+                <td>{orders.price}</td>
+                <td>{orders.calories}</td>
               </tr>
             ))}
           </tbody>
