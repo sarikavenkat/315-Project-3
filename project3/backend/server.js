b936e309e2455dd0755a2bda1627040e1c7cde8c
Paul <pauljwbae@gmail.com>
2023-12-04 06:29:50 -0600

    Tried changing addresses

--- project3/backend/server.js
+++ project3/backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json()); 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://3.22.171.17:5000'); 
+  res.header('Access-Control-Allow-Origin', 'http://172.31.32.166:5000'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
