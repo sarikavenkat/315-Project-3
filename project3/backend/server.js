96b2e24b7260424281ce163a56efde2e83ba229d
Paul <pauljwbae@gmail.com>
2023-12-10 22:24:12 -0600

    testing

--- project3/backend/server.js
+++ project3/backend/server.js
@@ -39,6 +39,10 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://pauljwbae.com'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  res.header('Access-Control-Allow-Origin', 'http://psill.com'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
